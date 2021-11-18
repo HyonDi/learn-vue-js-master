71bfeb20d056859b4eff36be09e458a70999f774
hjKang <develocal25@gmail.com>
2021-11-19 01:11:01 +0900

    20211119

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,25 +1,46 @@
 <template>
-  <div>
-    <form action="">
-      <div>
-        <label for="username">id: </label>
-        <input id="username" type="text" v-model="username">
-      </div>
-      <div>
-        <label for="password">pw: </label>
-        <input id="password" type="password" v-model="password">
-      </div>
-      <button type="submit">login</button>
-    </form>
-  </div>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id : </label>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw : </label>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function(){
     return {
-      username : '',
+      username: '',
       password: ''
+    }
+  },
+  methods: {
+    submitForm: function(){
+      //event.preventDefault();// 새로고침을 막고있다.
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+      // http 프로토콜을 이용한 통신라이브러리.
+      // 브라우저에서 서버로 데이터를 주고받기위한 라이브러리.
+      console.log(this.username, this.password);
     }
   }
 }
